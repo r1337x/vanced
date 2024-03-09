1a6b746fbb02cf3d37ac12c6f49f2e13413ca0d7
1337x <71149118+r1337x@users.noreply.github.com>
2024-03-09 23:46:17 +0000

    Update config.py

--- telegram-script/config.py
+++ telegram-script/config.py
@@ -1,6 +1,6 @@
 class Config:
     REVANCED_APKS_RELEASE_URL = (
-        "https://api.github.com/repos/revanced-apks/build-apps/releases/latest"
+        "https://api.github.com/repos/r1337x/vanced/releases/latest"
     )
     MICROG_RELEASE_URL = (
         "https://api.github.com/repos/inotia00/VancedMicroG/releases/latest"
@@ -12,7 +12,7 @@ class Config:
         "https://api.github.com/repos/inotia00/revanced-patches/compare"
     )
     
-    CREDITS_MESSAGE = "Credits to our upstream repository [j-hc/revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)"
+    CREDITS_MESSAGE = "Based on ReVanced Stable"
 
     RELEASE_MESSAGE = """📑 *RELEASE* {release_name}
 
@@ -24,4 +24,4 @@ class Config:
 
 {credits_message}
     
-@revanced_apks_web | revanced-apks.pages.dev"""
+1337x"""
